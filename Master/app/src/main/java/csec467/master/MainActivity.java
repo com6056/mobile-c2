ae9ae2b096edc47f18d1d45735b98c45743e71cc
Jordan Rodgers <com6056@gmail.com>
2017-10-01 00:24:33 -0400

    add sample pubnub code for master

--- Master/app/src/main/java/csec467/master/MainActivity.java
+++ Master/app/src/main/java/csec467/master/MainActivity.java
@@ -3,6 +3,14 @@ package csec467.master;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +18,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    PNConfiguration pnConfiguration = new PNConfiguration();
+
+    protected void publish() {
+        pnConfiguration.setSubscribeKey("SubscribeKey");
+        pnConfiguration.setPublishKey("PublishKey");
+        pnConfiguration.setSecure(false);
+
+        PubNub pubnub = new PubNub(pnConfiguration);
+
+        pubnub.subscribe()
+                .channels(Arrays.asList("my_channel")) // subscribe to channels
+                .execute();
+
+        pubnub.publish()
+                .message(Arrays.asList("hello", "there"))
+                .channel("my_channel")
+                .shouldStore(true)
+                .usePOST(true)
+                .async(new PNCallback<PNPublishResult>() {
+                    @Override
+                    public void onResponse(PNPublishResult result, PNStatus status) {
+                        if (status.isError()) {
+                            // something bad happened.
+                            System.out.println("error happened while publishing: " + status.toString());
+                        } else {
+                            System.out.println("publish worked! timetoken: " + result.getTimetoken());
+                        }
+                    }
+                });
+    }
+
+
 }

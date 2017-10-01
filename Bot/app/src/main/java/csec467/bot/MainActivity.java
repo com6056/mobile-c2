2d19cf2d50378fcfc56cb9475fee005d37947f57
Jordan Rodgers <com6056@gmail.com>
2017-09-30 22:42:53 -0400

    created empty projects

---
+++ Bot/app/src/main/java/csec467/bot/MainActivity.java
@@ -0,0 +1,13 @@
+package csec467.bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

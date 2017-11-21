e2ad20156d224498d430adbecdbf089698d4b07d
Jordan Rodgers <com6056@gmail.com>
2017-11-20 23:21:39 -0500

    bot app can receive background and foreground notifications and act on them

--- Bot/app/src/main/java/csec467/bot/MainActivity.java
+++ Bot/app/src/main/java/csec467/bot/MainActivity.java
@@ -1,12 +1,19 @@
 package csec467.bot;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,21 +21,96 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button increment = (Button)findViewById(R.id.increment);
-        View.OnClickListener ev = new View.OnClickListener() {
+
+        SharedPreferences sharedPref = this.getSharedPreferences("corgi", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPref.edit();
+        String corgi_counter = sharedPref.getString("corgi_counter", null);
+
+        Button corgiBtn = (Button) findViewById(R.id.corgiBtn);
+        final TextView corgiTxt = (TextView) findViewById(R.id.corgiTxt);
+
+        if (corgi_counter == null) {
+            corgi_counter = "0 Corgis!";
+            corgiTxt.setText(corgi_counter);
+        } else {
+            corgiTxt.setText(corgi_counter);
+        }
+
+        if (getIntent().getExtras() != null) {
+            if (getIntent().getExtras().get("corgis") != null) {
+                addBonus(getIntent());
+            }
+//            for (String key : getIntent().getExtras().keySet()) {
+//                Object value = getIntent().getExtras().get(key);
+//                Toast.makeText(this, "Key: " + key + " Value: " + value, Toast.LENGTH_LONG).show();
+//            }
+        }
+
+        View.OnClickListener corgi_clicker = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView corgi_text = (TextView)findViewById(R.id.corgis_text);
-                String text = corgi_text.getText().toString();
-                String[] text_array = text.split(" ");
-                Integer count = Integer.parseInt(text_array[0]);
-                Integer new_value = count + 1;
-                String newString = new_value.toString() + " " + text_array[1];
-                corgi_text.setText(newString);
+                Integer cur_count = Integer.parseInt(corgiTxt.getText().toString().split(" ")[0].replace(",", ""));
+                Integer new_count = cur_count + 1;
+                if (new_count == 1) {
+                    String new_text = NumberFormat.getNumberInstance(Locale.US).format(new_count) + " Corgi!";
+                    corgiTxt.setText(new_text);
+                    editor.putString("corgi_counter", new_text).apply();
+                } else {
+                    String new_text = NumberFormat.getNumberInstance(Locale.US).format(new_count) + " Corgis!";
+                    corgiTxt.setText(new_text);
+                    editor.putString("corgi_counter", new_text).apply();
+                }
             }
         };
-        increment.setOnClickListener(ev);
+        corgiBtn.setOnClickListener(corgi_clicker);
     }
 
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getExtras() != null) {
+                if (intent.getExtras().getString("corgis") != null) {
+                    addBonus(intent);
+                }
+            }
+        }
+    };
 
+    @Override
+    protected void onResume() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("csec467.bot.BONUS_CLICKS");
+        registerReceiver(receiver, filter);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(receiver);
+        super.onPause();
+    }
+
+    public void addBonus(Intent intent) {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("corgi", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        String corgi_counter = sharedPref.getString("corgi_counter", null);
+        TextView corgiTxt = (TextView) findViewById(R.id.corgiTxt);
+
+        if (corgi_counter == null) {
+            corgi_counter = "0 Corgis!";
+            corgiTxt.setText(corgi_counter);
+        } else {
+            corgiTxt.setText(corgi_counter);
+        }
+
+        Integer cur_count = Integer.parseInt(corgi_counter.split(" ")[0].replace(",", ""));
+        Integer bonus_count = Integer.parseInt(intent.getExtras().getString("corgis"));
+        Integer new_count = cur_count + bonus_count;
+        String new_text = NumberFormat.getNumberInstance(Locale.US).format(new_count) + " Corgis!";
+        corgiTxt.setText(new_text);
+        editor.putString("corgi_counter", new_text).apply();
+        Toast.makeText(getApplicationContext(), "Congrats! You have earned " + NumberFormat.getNumberInstance(Locale.US).format(bonus_count) + " Corgi Clicks!", Toast.LENGTH_LONG).show();
+    }
 }
+
+
--- Bot/app/src/main/java/csec467/bot/MyFCMService.java
+++ Bot/app/src/main/java/csec467/bot/MyFCMService.java
@@ -1,5 +1,6 @@
 package csec467.bot;
 
+import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -10,32 +11,22 @@ public class MyFCMService extends FirebaseMessagingService {
     String TAG = "abc123";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-//                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-//                handleNow();
+            if (remoteMessage.getData().get("corgis") != null) {
+                Log.d(TAG, "corgis: " + remoteMessage.getData().get("corgis"));
+                Intent bonus = new Intent("csec467.bot.BONUS_CLICKS");
+                bonus.putExtra("corgis", remoteMessage.getData().get("corgis"));
+                sendBroadcast(bonus);
             }
 
         }
 
-        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 }
